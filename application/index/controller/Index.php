f39f7da3217d882d0efe7460f5687934f8f507c4
tong <tong@qq.com>
2019-03-05 23:45:21 +0800

    .

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,12 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+
 class Index extends Controller
 {
     public function index()
     {
-        return $this->fetch('index');
+        return $this->fetch();
     }
 
     // public function hello($name = 'ThinkPHP5')
