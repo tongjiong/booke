8ba661e2d5369d7ab666997aa43dc62529207d95
tong <tong@example.com>
2019-03-05 15:12:08 +0800

    .

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,7 +5,7 @@ class Index
 {
     public function index()
     {
-        return '<h2 style="color:red;">猴子借钱同炯，不借直杰，天理不容！</h2>';
+        return '<h2 style="color:red;">弘德 请他吃饭 请他睡觉 请他抽烟 叫帮我做张图都不给我做，天理不容!</h2>';
     }
 
     public function hello($name = 'ThinkPHP5')
